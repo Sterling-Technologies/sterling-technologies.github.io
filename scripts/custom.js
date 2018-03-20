347da5549f96b53e97ec7b73b329154162ce47c3
Christian Blanquera <cblanquera@openovate.com>
2018-03-21 02:45:45 +0800

    boilerplate

---
+++ scripts/custom.js
@@ -0,0 +1,14 @@
+jQuery(function($) {
+    /*$(window).on('fixed-scroll-init', function(e, target) {
+        var clone = $(target).clone().addClass('fixed-scroll');
+        $(window).scroll(function(e) {
+            if($(this).scrollTop() > 100) {
+                clone.appendTo(document.body);
+            } else {
+                clone.remove();
+            }
+        });
+    });*/
+
+    $(document.body).doon();
+});
